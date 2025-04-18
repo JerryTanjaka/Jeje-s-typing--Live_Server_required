9cfbba75347379c6620a827207f5b6bb3160e9f9
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-04-19 01:18:19 +0300

    add : some theme on settings

--- ASSETS/js/settings.js
+++ ASSETS/js/settings.js
@@ -1,41 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const music = document.getElementById('backgroundMusic');
-
-    const toggles = document.querySelectorAll('.flex.items-center.justify-between');
-
-    let soundToggle = null;
-
-    toggles.forEach((toggle) => {
-        const span = toggle.querySelector('span');
-        if (span && span.textContent.trim() === "Sound") {
-            soundToggle = toggle.querySelector('input[type="checkbox"]');
-        }
-    });
-
-    if (!soundToggle) return;
-
-    const storedSoundSetting = localStorage.getItem('soundSetting');
-    if (storedSoundSetting !== null) {
-        soundToggle.checked = JSON.parse(storedSoundSetting);
-    }
-
-    if (soundToggle.checked) {
-        music.volume = 0.5;
-        music.play().catch(e => console.log("Erreur lecture audio :", e));
-    } else {
-        music.pause();
-        music.currentTime = 0;
-    }
-
-    soundToggle.addEventListener('change', () => {
-        localStorage.setItem('soundSetting', JSON.stringify(soundToggle.checked));
-
-        if (soundToggle.checked) {
-            music.volume = 0.5;
-            music.play().catch(e => console.log("Erreur lecture audio :", e));
-        } else {
-            music.pause();
-            music.currentTime = 0;
-        }
-    });
+const parent = document.querySelector(".parent");
+const settingsSelector = document.querySelector(".settingsSelector");
+const settings = document.querySelector(".settings")
+const exit = document.querySelector("#exit")
+settingsSelector.addEventListener("click", () => {
+    parent.classList.add("blur-sm");
+    settings.classList.add("absolute","top-0")
+    exit.classList.remove("opacity-0")
+    
 });
+exit.addEventListener("click",()=>{
+
+    parent.classList.remove("blur-sm");
+    settings.classList.remove("absolute","top-0")
+    exit.classList.add("opacity-0")
+
+})
