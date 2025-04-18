44a1984b426a9c3c4eb9c414ad392cc749d20971
Jessica <jessicakantotiana@gmail.com>
2025-04-18 12:08:10 +0100

    update : merge typing-dashboard changes in the branch dev

--- exercises.js
+++ exercises.js
@@ -3,12 +3,13 @@ let interval = null;
 
 const inputField = document.getElementById("user-input");
 const display = document.getElementById("text-to-type");
-const wpmDisplay = document.querySelector(".wpm");
-const accuracyDisplay = document.querySelector(".accuracy");
-const timeDisplay = document.querySelector(".time");
+const wpmDisplay = document.querySelector("#wpm-value");
+const accuracyDisplay = document.querySelector("#accuracy-value");
+const timeDisplay = document.querySelector("#time-value");
 
 const targetText = 'fj fj fj fj j fj fj fj j fj fj fj j fj fj fj j fj fj fj';
 
+// Démarrage du chronomètre - setInterval() est une fonction qui exécute une autre toute les X milisecondes 
 function startTimer() {
     if (!startTime) {
         startTime = Date.now();
@@ -16,22 +17,25 @@ function startTimer() {
     }
 }
 
+// Mise à jour de WPM et de l'accuracy 
 function updateStats() {
     const elapsedSeconds = (Date.now() - startTime) / 1000;
     const elapsedMinutes = elapsedSeconds / 60;
     const typedText = inputField.value;
 
+    // Comptage des caractères corrects tapés par l'utilisateur
     let correctChars = 0;
     for (let i = 0; i < typedText.length; i++) {
         if (typedText[i] === targetText[i]) {
             correctChars++;
         }
     }
-
+    // Calcul en pourcentage de la valeur de l'accuracy 
     const accuracy = typedText.length > 0
         ? (correctChars / typedText.length) * 100
         : 0;
 
+    // Calcul du WPM. PS : 5 caractères pour repère de validation 
     const wpm = elapsedMinutes > 0
         ? (typedText.length / 5) / elapsedMinutes
         : 0;
@@ -41,13 +45,14 @@ function updateStats() {
     timeDisplay.textContent = `${Math.floor(elapsedSeconds)}s`;
 }
 
+// Coloration des caractère corrects ou incorrects saisis par l'utilisateur 
 function updateColoredText() {
     const typed = inputField.value;
     let result = '';
 
     for (let i = 0; i < targetText.length; i++) {
-        const char = targetText[i];
-        const userChar = typed[i];
+        const char = targetText[i];         // Texte à saisir : fj fj fj...
+        const userChar = typed[i];          // Texte saisi par l'utilisateur 
 
         if (userChar == null) {
             result += `<span>${char}</span>`;
@@ -61,6 +66,7 @@ function updateColoredText() {
     display.innerHTML = result;
 }
 
+// Démarrage du typing-test au moment de l'input de l'utilisateur
 document.addEventListener('DOMContentLoaded', () => {
     updateColoredText(); 
     
@@ -69,4 +75,25 @@ document.addEventListener('DOMContentLoaded', () => {
         updateColoredText();
         updateStats();
     });
-});
+}); 
+
+// Redémarrage de tous les compteurs à 0 avec le bouton RESTART
+document.addEventListener("DOMContentLoaded", function (){
+    const restart = document.getElementById("restartButton")
+    restart.addEventListener("click", function (){
+        // Stoppe le timer
+        clearInterval(interval);
+    
+        // Réinitialiser toutes les précédentes valeurs
+        startTime = null;
+        interval = null;
+    
+        // Remettre le compteur et les stats à zéro
+        document.getElementById("time-value").textContent = "0s";
+        document.getElementById("accuracy-value").textContent = "0%"
+        document.getElementById("wpm-value").textContent = "0"
+    
+        // Vider le champ de saisie
+        document.getElementById("user-input").value = "";
+    })
+})
