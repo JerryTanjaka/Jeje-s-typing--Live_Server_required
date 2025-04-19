3218e1ed39c7f230fae93dd2ccf9c3e05b1576a4
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-04-19 16:39:09 +0300

    debug : redirection to another page

--- ASSETS/js/redirection.js
+++ ASSETS/js/redirection.js
@@ -3,10 +3,15 @@ window.addEventListener("DOMContentLoaded", () => {
     const info = document.getElementById("info");
     const lesson1 = document.querySelector(".lesson1");
     const dash = document.getElementById("todashboard");
-
+    const typing = document.getElementById("typing")
     if (lesson1) {
         lesson1.addEventListener("click", () => {
             window.location.href = "exo/lesson1.html";
+        });
+    }
+    if (typing) {
+        typing.addEventListener("click", () => {
+            window.location.href = "typing.html";
         });
     }
 
